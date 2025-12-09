47a683467efb171ae2342d1e024f879d9ede1478
Xscrypto001 <ikinyapeter93@gmail.com>
2025-12-09 20:03:45 +0300

    fix: prediction market.sol

--- smart_contracts/hardhat.config.mjs
+++ smart_contracts/hardhat.config.mjs
@@ -5,7 +5,7 @@ export default {
   networks: {
     sepolia: {
       url: "https://base-goerli.infura.io/v3/3e6eaf4b16e4401c8773515091f08229",
-      accounts: ["cfcf932c9f9e690dbdaaa471e3481fc732b614307b70f20337d9fcfbf5d16607"]
+      accounts: ["zfcf932c9f9e690dbdaaa471e3481fc732b614307b70f20337d9fcfbf5d16607"]
     }
   }
 };
