28b2eda6aefbe7ff30df94b2b52626173aa074db
Xscrypto001 <ikinyapeter93@gmail.com>
2025-12-09 19:44:13 +0300

    fix: ui wallet cnfig context

--- frontend-market/vite.config.js
+++ frontend-market/vite.config.js
@@ -1,25 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-=======
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
->>>>>>> main
 })
